47fa9370ac89065e7714ddfdc8db2fb70778fb79
Emily Chen <tenshiemi@gmail.com>
2019-04-02 08:43:16 -0700

    Add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Widget Quality Control Module
+
+# Assumptions
+* Data will always have the same format as the sample data
+* Only temperature (t) and humidity (h) are evaluated with this module
+* Mean is not rounded, results would vary slightly if rounded to the 10ths place like the input data
+
+
+# Wishlist
+* Error handling
+* Tests
---
+++ index.js
@@ -0,0 +1,58 @@
+function calculateMean(array) {
+  return array.reduce((acc, value) => {
+    return acc + value;
+  }, 0) / array.length;
+}
+
+function calculateStandardDeviation(array, mean) {
+  return Math.sqrt(array.reduce((acc, value) => {
+    return acc + Math.pow(value - mean, 2);
+  }, 0) / array.length);
+}
+
+function evaluateTemp(array, temp) {
+  const mean = calculateMean(array);
+  const stdDeviation = calculateStandardDeviation(array, mean);
+
+  const withinHalfDegree = Math.abs(mean - temp) <= 0.5;
+
+  if (withinHalfDegree && stdDeviation < 3) {
+    return 'ultra precise';
+  } else if (withinHalfDegree && stdDeviation < 5) {
+    return 'very precise';
+  } else {
+    return 'precise';
+  }
+}
+
+function evaluateHumidity(array, humidity) {
+  return array.every((dataPoint) => {
+    return Math.abs(dataPoint - humidity) <= 1;
+  }) ? 'keep' : 'discard';
+}
+
+function evaluateLogFile(data) {
+  const [, temp, humidity] = data.shift().split(' ');
+  const groupedData = {};
+  const result = {};
+
+  let currentCategory;
+
+  for (let line = 0; line < data.length; line++) {
+    if (data[line][0] === 't' || data[line][0] === 'h') {
+      currentCategory = data[line].split(' ')[1];
+      groupedData[currentCategory] = [];
+    } else {
+      groupedData[currentCategory].push(parseFloat(data[line].split(' ')[1]));
+    }
+  };
+
+  for (let category in groupedData) {
+    result[category] = category[0] === 't' ?
+      evaluateTemp(groupedData[category], temp) : evaluateHumidity(groupedData[category], humidity);
+  }
+
+  return result;
+}
+
+module.exports = evaluateLogFile;
